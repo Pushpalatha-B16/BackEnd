4626b845287614153762f4440781bf9a7b30f11e
Pushpalatha-B16 <pushpalathabalaji1234@gmail.com>
2024-10-31 10:08:23 +0530

    Add files via upload

---
+++ patterrn problems/halfpyramid.py
@@ -0,0 +1,23 @@
+#simple pyramid pattern(half pyramid)
+"""
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * """
+
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+      print("*",end=" ")
+    print()
+
+
+
+
+ 
+
+
+
